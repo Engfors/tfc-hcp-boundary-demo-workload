8cef510f2ed29fb1135f45be911458e113d50370
JoeStack <joern@hashicorp.com>
2023-07-28 13:47:43 +0200

    typo fixed 2.1

--- templates/worker.sh
+++ templates/worker.sh
@@ -25,7 +25,7 @@ listener "tcp" {
   purpose = "proxy"
 }
 
-hcp_boundary_cluster_id = $( echo ${boundary_cluster_addr%%.*} | cut -c 9- )
+hcp_boundary_cluster_id = ${boundary_cluster_addr}
 
 worker {
   public_addr = $(public_ip)
