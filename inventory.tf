9007b867190fc6dcfe35d89d833c1b5cf2b4e282
JoeStack <joern@hashicorp.com>
2023-07-31 16:12:32 +0200

    test static proj_id

--- inventory.tf
+++ inventory.tf
@@ -139,7 +139,7 @@ resource "null_resource" "ansible_run" {
   provisioner "remote-exec" {
     inline = [
       "echo 'ssh is up...'",
-      "sleep 60 && ansible-playbook -i ~/inventory ~/ansible/playbook.yml ",
+      "sleep 60 && ansible-playbook -i /home/ubuntu/inventory /home/ubuntu/ansible/playbook.yml ",
     ]
   }
 }
